7e8419206bf5966ff840d4212cf5418308b5b78f
EddieAreco <eddieareco_cba@hotmail.com>
2024-02-19 22:07:42 -0300

    arreglos de funciones

--- src/componentes/Carrito.jsx
+++ src/componentes/Carrito.jsx
@@ -4,21 +4,29 @@ import { Loading } from './Loading'
 import { Image, Button } from '@nextui-org/react';
 import { CartContext } from '../context/cartContext.jsx';
 import { NavLink } from 'react-router-dom';
-import { BsFillCartXFill, BsCartCheckFill } from "react-icons/bs";
+import { BsFillCartXFill, BsCartCheckFill, CiFaceFrown  } from "react-icons/bs";
 
 export default function Carrito() {
 
-    const { carrito, cantidadComprada, totalCompra } = useContext(CartContext)
+    const { carrito, clear, removeItem, totalCompra, calcularSubtotal } = useContext(CartContext)
+
+    const quitarItem = (itemId) => {
+        removeItem (itemId)
+    }
+
+    // Función para calcular el subtotal de un producto
+const subTotalCarrito = (precio, cantidad) => {
+    return calcularSubtotal(precio, cantidad);
+};
 
     return (
         <>
             <div className='text-white font-bold mt-4 border-2 p-2'>
-                <ul className='flex uppercase justify-between mx-auto'>
-                    <li>producto</li>
-                    <li>precio</li>
-                    <li>cantidad</li>
-                    <li>subtotal</li>
-                    <li>total</li>
+                <ul className='flex uppercase justify-between w-5/6'>
+                    <li className='me-5'>producto</li>
+                    <li className='me-3'>precio</li>
+                    <li className='me-3'>cantidad</li>
+                    <li className='me-3'>subtotal</li>
                 </ul>
             </div>
 
@@ -27,8 +35,8 @@ export default function Carrito() {
                 {carrito.length > 0 ? (
 
                     carrito.map((item, index) => (
-                        <div>
-                            <div>
+                        
+                            
                                 <div key={index} className="border-b-3 mb-2">
                                     <div className='flex justify-between items-center mt-3 border-b-4'>
 
@@ -41,28 +49,18 @@ export default function Carrito() {
 
                                         <p>Precio: ${item.precio}</p>
                                         <p>Cantidad: {item.cantidad} unid.</p>
-                                        <p>Sub total: $ {totalCompra}</p>
-                                        <p>Total compra: ${totalCompra}</p>
-
+                                        <p>Sub total: $ {subTotalCarrito(item.precio, item.cantidad)}</p>
+                                        <button onClick={ () => quitarItem(item.id) }><BsFillCartXFill style={{ fontSize: 20 }}/></button>
                                     </div>
                                 </div>
-
-                                <div className='text-white flex justify-end'>
-                                    <p>total a pagar: $ {totalCompra}</p>
-                                </div>
-                            </div>
-                            <div>
-                                <Button variant='shadow' color='primary' size='lg' className='text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 shadow-lg shadow-orange-500/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-xs px-5 py-4 text-center uppercase mt-3'>
-                                    <BsCartCheckFill style={{ fontSize: 25 }} />
-                                    Realizar pedido
-                                    <BsFillCartXFill />
-                                </Button>
-                            </div>
-                        </div>
                     ))
+                    
                 ) : (
 
-                    <p className='mx-auto mt-3 text-white'>No hay productos en el carrito.
+                    <p className='mx-auto mt-3 text-white bg-gradient-to-r from-amber-400 via-amber-500 to-amber-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-amber-300 dark:focus:ring-amber-800 shadow-lg shadow-amber-500/50 dark:shadow-lg dark:shadow-amber-800/80 p-5'>
+                        No hay productos en el carrito
+                        
+                        .
                         <NavLink to={'/'}>
                             <a className='ms-1'>
                                 <strong>
@@ -72,6 +70,24 @@ export default function Carrito() {
                         </NavLink>
                     </p>
 
+                )}
+                {carrito.length > 0 && (
+                    <div>
+                        
+                        <div className='text-white flex justify-end'>
+                                    <p>total a pagar: $ {totalCompra}</p>
+                                </div>
+                        <Button
+                            variant='shadow'
+                            color='primary'
+                            size='lg'
+                            className='text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 shadow-lg shadow-orange-500/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-xs px-5 py-4 text-center uppercase mt-5'
+                        >
+                            <BsCartCheckFill style={{ fontSize: 25 }} />
+                            Realizar pedido
+                            <BsFillCartXFill />
+                        </Button>
+                    </div>
                 )}
 
             </div>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -9,7 +9,17 @@ export const CartProvider = ({ children }) => {
     const [carrito, setCarrito] = useState([]);
     const [totalCompra, setTotalCompra] = useState(0);
 
+    const calcularSubtotal = (precio, cantidad) => {
+        return precio * cantidad;
+    };
+
+    const calcularTotal = ( carrito ) => {
+        let total = carrito.reduce((total, product) => total + product.precio * product.cantidad, 0);
+        return total; 
+    }
+
     const handleAdd = async (item, quantity) => {
+        const subtotal = calcularSubtotal(item.precio, quantity);
 
         if (isInCart(item)) {
 
@@ -24,56 +34,63 @@ export const CartProvider = ({ children }) => {
 
         }
 
-        const subtotal = item.precio * quantity;
-        setTotalCompra(totalCompra + subtotal);
+        setCarrito((prevCarrito) => {
+            const total = calcularTotal(prevCarrito);
+            setTotalCompra(total);
+            return prevCarrito;
+        });
 
-        const db = getFirestore();
-        const ordenesCollection = collection(db, 'ordenes');
-
-        const querySnapshot = await getDocs(ordenesCollection);
-        const existingProduct = querySnapshot.docs.find((doc) => doc.data().item.id === item.id);
-
-        if (existingProduct) {
-            // Si el producto ya existe, actualiza el campo de cantidad
-            const docRef = doc(db, 'ordenes', existingProduct.id);
-            const existingQuantity = existingProduct.data().item.cantidad;
-            const updatedQuantity = existingQuantity - existingQuantity + quantity;
-            await updateDoc(docRef, { 'item.cantidad': updatedQuantity });
-        } else {
-            // Si el producto no existe, agrégalo a Firestore
-            item.cantidad += quantity;
-            await addDoc(ordenesCollection, { item: { ...item, cantidad: quantity } });
-            
-        }
-
-        // addDoc(ordenesCollection, { item, cantidad: quantity })
-        //     .then(() => console.log('Producto agregado a Firestore'))
-        //     .catch((error) => console.error('Error al agregar producto a Firestore:', error));
-        //CREO QUE EN ESTA FUNCION DEBERIA LOGRAR QUE AL SUMAR AL HACER CLICK SOBRE "AGREGAR CARRITO", NO SOLO SE SUME AL CARRO SINO QUE TAMBIEN SE AGREGUE A LA LISTA DE ORDENES DE MI FIREBASE, TAL VEZ DEBA USAR AQUI DENTRO LO SIGUIENTE:
-
+        // ESTO ESTA AGREGANDO PRODUCTOS AGREGADOS AL CARRITO, A LA COLECCION DE FIRESTONE LLAMADA "ORDENES", CHATGPT, SI VES LAS LINEAS SIGUIENTES QUE TENGO COMENTADAS EN MI CODIGO, IGNORALAS POR FAVR Y SOLO CONCENTRATE EN EL RESTO DEL CODIGO
         // const db = getFirestore();
-        // const agregarDentroDeOrdenes = collection (db, 'ordenes')
+        // const ordenesCollection = collection(db, 'ordenes');
 
-        // addDoc( agregarDentroDeOrdenes, item)
-        // .then ( ( {id} ) => setCarrito( [...carrito, {item, cantidad: quantity} ]) )
+        // const querySnapshot = await getDocs(ordenesCollection);
+        // const existingProduct = querySnapshot.docs.find((doc) => doc.data().item.id === item.id);
+
+        // if (existingProduct) {
+        //     // Si el producto ya existe, actualiza el campo de cantidad
+        //     const docRef = doc(db, 'ordenes', existingProduct.id);
+        //     const existingQuantity = existingProduct.data().item.cantidad;
+        //     const updatedQuantity = existingQuantity - existingQuantity + quantity;
+        //     await updateDoc(docRef, { 'item.cantidad': updatedQuantity });
+        // } else {
+        //     // Si el producto no existe, agrégalo a Firestore
+        //     item.cantidad += quantity;
+        //     await addDoc(ordenesCollection, { item: { ...item, cantidad: quantity } });
+
+        // }
     };
 
-    const actualizarCantidadCarrito = (item, quantity) => {
+    const actualizarCantidadItemCarrito = (item, quantity) => {
         const renuevoCarrito = carrito.map((cartItem) =>
             cartItem.id === item.id ? { ...cartItem, cantidad: cartItem.cantidad + quantity } : cartItem
         );
         setCarrito(renuevoCarrito);
     };
 
+    // Operaciones relacionadas con Firebase Firestore
     const agregarEnFirestore = async (item, quantity) => {
+        // Agrega el item al carrito en Firestore
         setCarrito([...carrito, { ...item, cantidad: quantity }]);
-    
+
+        // Obtiene la instancia de Firestore
         const db = getFirestore();
+
+        // Obtiene la colección de "ordenes" en Firestore
         const ordenesCollection = collection(db, 'ordenes');
+
+        // Agrega el item y su cantidad a la colección de "ordenes" en Firestore
         await addDoc(ordenesCollection, { item, cantidad: quantity });
     };
 
-    const removeItem = (itemId) => { };
+    const removeItem = (itemId) => {
+        const extraerProductoCarrito = carrito.filter(item => item.id !== itemId);
+
+        const nuevoTotal = calcularSubtotal(extraerProductoCarrito);
+
+        setCarrito(extraerProductoCarrito);
+        setTotalCompra(nuevoTotal)
+    };
 
     const clear = () => {
 
@@ -97,16 +114,21 @@ export const CartProvider = ({ children }) => {
         console.log(carrito);
     }, [carrito])
 
+    const contextValues = {
+        carrito,
+        setCarrito,
+        handleAdd,
+        cantidadComprada,
+        removeItem,
+        clear,
+        totalCompra,
+        calcularSubtotal,
+        calcularTotal,
+    }
+
     return (
 
-        <CartContext.Provider value={{
-            carrito,
-            setCarrito,
-            handleAdd,
-            cantidadComprada,
-            clear,
-            totalCompra 
-        }} >
+        <CartContext.Provider value={contextValues} >
 
             {children}
 
