fe62b09f6834e6fa9b08748482fee939b3b72ad3
EddieAreco <eddieareco_cba@hotmail.com>
2024-03-23 18:54:33 -0300

    Updates

--- src/componentes/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import {Spinner} from "@nextui-org/react";
-
-export const Loading = () =>{
-
-    return (
-        <Spinner label="Aguarde por favor..." color="warning" labelColor="warning" className="mt-5"/>
-      );
-
-}
